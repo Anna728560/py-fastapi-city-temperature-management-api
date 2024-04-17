60831bc62beadafa4122e387c41f321df72dce30
Anya <xxxuko@gmail.com>
2024-04-17 11:18:53 +0300

    some fixes in temp_crud and add env_sample with api key

--- crud/temp_crud.py
+++ crud/temp_crud.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 import models
 import database
-import schemas
+
 
 
 def get_all_temperatures(db: Session = Depends(database.get_db)):
@@ -18,11 +18,31 @@ def get_one_temperature_by_city_id(
     temperature = db.query(models.Temperature).filter(
         models.Temperature.city_id == city_id
     ).first()
+
+    if not temperature:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"City with the id {city_id} does not exist"
+        )
+
     return temperature
 
 
 def update_all_temperatures(
        db: Session = Depends(database.get_db)
 ):
+    cities = db.query(models.City).all()
 
+    for city in cities:
+        #  api weather logic
+        temperature_data = {
+            "temperature": 25.5
+        }
+        new_temperature = models.Temperature(
+            city_id=city.id,
+            temperature=temperature_data["temperature"]
+        )
+        db.add(new_temperature)
+
+    db.commit()
     return "Temperature data updated successfully"
