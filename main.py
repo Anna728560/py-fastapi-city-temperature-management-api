7b3421df4badccc7ee0a28e80a1ffde4a28885ac
Anya <xxxuko@gmail.com>
2024-04-17 09:03:16 +0300

    add create_city endpoint in main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, HTTPException
+
+from sqlalchemy.orm import Session
+
+from . import models, schemas, database
+from database import SessionLocal
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def create():
-    return {"message": "Hello World"}
+@app.get("/cities/", status_code=status.HTTP_201_CREATED)
+def create_city(
+        request: schemas.CitySchema,
+        db: Session = Depends(database.get_db),
+):
+    db_city = models.City(
+        name=request.name,
+        additional_info=request.additional_info,
+    )
+    db.add(db_city)
+    db.commit()
+    db.refresh(db_city)
+    return db_city
