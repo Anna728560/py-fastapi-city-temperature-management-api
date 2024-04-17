a7c65838b24d54a5a9e881d6683d92ccb0c1f390
Anya <xxxuko@gmail.com>
2024-04-17 11:13:53 +0300

    create routers for temperature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 import models
-from routers import city_routers
+from routers import city_routers, temp_routers
 import database
 
 
@@ -10,3 +10,4 @@ app = FastAPI()
 models.Base.metadata.create_all(database.engine)
 
 app.include_router(city_routers.router)
+app.include_router(temp_routers.router)
---
+++ routers/temp_routers.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, status
+
+from sqlalchemy.orm import Session
+
+import schemas
+import database
+from crud.temp_crud import (
+    get_all_temperatures,
+    get_one_temperature_by_city_id,
+    update_all_temperatures,
+)
+
+
+router = APIRouter(
+    tags=["Temperatures"],
+    prefix="/temperatures"
+)
+
+
+@router.post("/update/")
+def update_temperatures(db: Session = Depends(database.get_db)):
+    return update_all_temperatures(db=db)
+
+
+@router.get(
+    "/",
+    response_model=List[schemas.Temperature]
+)
+def get_temperatures(db: Session = Depends(database.get_db)):
+    return get_all_temperatures(db=db)
+
+
+@router.get("/{city_id}/")
+def get_temperature_by_city(
+        city_id: int,
+        db: Session = Depends(database.get_db)
+):
+    return get_one_temperature_by_city_id(city_id=city_id, db=db)
+
