d23acc722002ef1dd24c647feef8cd280fedfb48
Anya <xxxuko@gmail.com>
2024-04-17 09:20:43 +0300

    create get_city endpoint

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI, Depends, status, HTTPException
 
 from sqlalchemy.orm import Session
 
-from . import models, schemas, database
+import models
+import schemas
+import database
 from database import SessionLocal
 
 
 app = FastAPI()
 
 
-@app.get("/cities/", status_code=status.HTTP_201_CREATED)
+@app.post("/cities/", status_code=status.HTTP_201_CREATED)
 def create_city(
         request: schemas.CitySchema,
         db: Session = Depends(database.get_db),
@@ -22,3 +24,18 @@ def create_city(
     db.commit()
     db.refresh(db_city)
     return db_city
+
+
+@app.get("/cities/{id}", status_code=status.HTTP_200_OK)
+def get_city(
+        city_id: int,
+        db: Session = Depends(database.get_db)
+):
+    city = db.query(models.City).filter(models.City.id == city_id).first()
+    if not city:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"City with the id {city_id} does not exist"
+        )
+
+    return city
