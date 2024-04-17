de00a840ab5f1ac11aabe90ee927c409669bf08e
Anya <xxxuko@gmail.com>
2024-04-17 08:45:42 +0300

    create City and Temperature models

---
+++ models.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    Float
+)
+
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    additional_info = Column(String)
+
+    temperatures = relationship("Temperature", back_populates="city")
+
+
+class Temperature(Base):
+    __tablename__ = "temperatures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date_time = Column(DateTime)
+    temperature = Column(Float)
+    city_id = Column(Integer, ForeignKey("cities.id"))
+
+    city = relationship("City", back_populates="temperatures")
