fc04ce00654095fa2663009b83c1d53b1bf2952c
Anya <xxxuko@gmail.com>
2024-04-18 13:11:47 +0300

    fix bland line

--- db/database.py
+++ db/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./city.db"
 
 engine = create_engine(
